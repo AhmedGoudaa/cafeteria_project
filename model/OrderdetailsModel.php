a48720a683bdd7f69a3f4dd295ef50cfc4dc69c9
MusstafaA <mustafa.cu13@yahoo.com>
2016-02-29 13:52:39 -0800

    admin orders panel added

--- model/OrderdetailsModel.php
+++ model/OrderdetailsModel.php
@@ -8,6 +8,7 @@ class OrderdetailsModel extends Database {
     public $order_id;
     public $quantity;
     public $orderdetails;
+    public $user_ID;
     
     public $columns_names = "(order_id , product_id , quantity)";
 
@@ -34,6 +35,29 @@ class OrderdetailsModel extends Database {
 
      }
 
+
+
+    public function selectJoin(){
+
+       global $conn;
+
+       $sql = "SELECT od. product_id,o.user_id,p.name,p.photo,p.price FROM cafeteria.order_details od INNER JOIN cafeteria.order o on od.order_id=o.id INNER JOIN cafeteria.product p ON p.id=od.product_id where o.user_id=".$this->user_ID." GROUP BY od. product_id ORDER BY COUNT(*) DESC LIMIT 3;";
+
+
+       if ($res = mysqli_query($conn, $sql)) {
+          // echo "New record created successfully";
+        return $res ;
+       } else {
+           echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+       }
+
+       // mysqli_close($conn);
+
+
+     }
+
+
     
     
 }
+
