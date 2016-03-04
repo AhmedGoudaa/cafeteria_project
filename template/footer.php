f56c4e4d326f0dd964f965330ef929d569108da5
MusstafaA <mustafa.cu13@yahoo.com>
2016-03-04 07:10:20 -0800

    edit in design

--- template/footer.php
+++ template/footer.php
@@ -1,16 +1,37 @@
 </div>
 
-<footer id="footer" class="row content" style='background: url("<?= BASE_URL ?>static/img/banner2.jpg") no-repeat;'>
-    <div class='footer-text col-sm-4 col-md-4 col-lg-4 col-xs-4'>
-        <h5>About us</h5>
-    </div>
-    <div class='footer-text col-sm-4 col-md-4 col-lg-4 col-xs-4'>
-        <h5>Team</h5>
-    </div>
-    <div class='footer-text col-sm-4 col-md-4 col-lg-4 col-xs-4'>
-        <h5><i class="glyphicon glyphicon-copyright-mark"></i> Copyright 2016 GemyCafe All Rights Reserved</h5>
-    </div>
+
+<footer id="footer" class="row content footer-text" style='background: url("<?= BASE_URL ?>static/img/banner2.jpg") no-repeat;'>
+				<div  class="footer-section " >
+					<div class="container-fluid">
+						<div class="footer-grids">
+						<div class="col-md-6 footer-grid">
+							<h3>about us</h3>
+								<p>Gemy's new and expanded Cafe` represents a truly authentic drinks</p> 
+								GemyCafe the best Cafe` in Smart Village</p>
+								<p> Copyright &copy;2016  All rights  Reserved </p>
+							
+							</div>
+							<div class="col-md-3 footer-grid">
+							<h3>Opening Hours</h3>
+								<p><span>Mon-Thu:</span> 7:00am-8:00pm</p> 
+									<p><span>Fri-Sun:</span> 7:00am-4:30pm</p>
+									
+							</div>
+							<div class="col-md-3 footer-grid">
+							<h3>Our Location</h3>
+								<p>ITI-Smart Village</p>
+
+							</div>
+							<div class="clearfix"></div>
+							</div>
+
+						</div>
+					</div>
+
 </footer>
+
+
 </div>
 </body>
 </html>
